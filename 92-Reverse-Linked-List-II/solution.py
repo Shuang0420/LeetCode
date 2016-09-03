21409b477119561dc5710f9ae33d588737f7e053
Shuang0420 <252618408@qq.com>
2016-09-03 12:44:08 -0400

    [92. Reverse Linked List II][Accepted]committed by LeetCode Extension

---
+++ 92-Reverse-Linked-List-II/solution.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        if m == n:
+            return head
+
+        dummyNode = ListNode(0)
+        dummyNode.next = head
+        pre = dummyNode
+
+        for i in range(m - 1):
+            pre = pre.next
+        
+        # reverse the [m, n] nodes
+        reverse = None
+        cur = pre.next
+        for i in range(n - m + 1):
+            cur.next,reverse,cur=reverse,cur,cur.next
+
+        pre.next.next = cur
+        pre.next = reverse
+
+        return dummyNode.next
+        

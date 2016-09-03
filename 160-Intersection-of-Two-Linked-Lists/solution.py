d423324479726598dc2dfbfd28432ffa176e6e56
Shuang0420 <252618408@qq.com>
2016-09-03 17:13:41 -0400

    [160. Intersection of Two Linked Lists][Accepted]committed by LeetCode Extension

--- 160-Intersection-of-Two-Linked-Lists/solution.py
+++ 160-Intersection-of-Two-Linked-Lists/solution.py
@@ -10,11 +10,25 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        a, b = headA, headB
-        while a != b:
-            a = a.next if a else headB
-            b = b.next if b else headA
-        return a
+        curA,curB = headA,headB
+        lenA,lenB=0,0
+        while curA:
+            lenA += 1
+            curA = curA.next
+        while curB:
+            lenB += 1
+            curB = curB.next
+        curA,curB = headA,headB
+        if lenA > lenB:
+            for i in range(lenA-lenB):
+                curA = curA.next
+        else:
+            for i in range(lenB-lenA):
+                curB = curB.next
+        while curA != curB:
+            curA = curA.next
+            curB = curB.next
+        return curA
         
         
 

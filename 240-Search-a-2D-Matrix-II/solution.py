77ea461310d7517a32af39ad04ce3d52abae5d32
Shuang0420 <252618408@qq.com>
2016-09-20 17:00:15 -0400

    [240. Search a 2D Matrix II][Accepted]committed by LeetCode Extension

--- 240-Search-a-2D-Matrix-II/solution.py
+++ 240-Search-a-2D-Matrix-II/solution.py
@@ -7,19 +7,15 @@ class Solution(object):
         """
         if not matrix:
             return False
-        return self.helper(matrix,target,0,0,len(matrix)-1,len(matrix[0])-1)
-        
-        
-    def helper(self,matrix,target,startX,startY,endX,endY):
-        if startX>endX or startY>endY:
-            return False
-        midX=(startX+endX)/2
-        midY=(startY+endY)/2
-        mid = matrix[midX][midY]
-        if mid == target:
-            return True
-        if mid > target:
-            return self.helper(matrix,target,startX,midY,midX-1,endY) or self.helper(matrix,target,startX,startY,endX,midY-1)
-        else:
-            return self.helper(matrix,target,midX+1,startY,endX,midY) or self.helper(matrix,target,startX,midY+1,endX,endY)
-        return False
+        i,j = 0,len(matrix[0])-1
+        while True:
+            if j<0 or i>len(matrix)-1:
+                return False
+            curr = matrix[i][j]
+            if curr == target:
+                return True
+            if curr > target:
+                j -= 1
+            else:
+                i += 1
+            

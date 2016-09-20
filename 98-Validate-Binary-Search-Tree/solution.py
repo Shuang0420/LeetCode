749f13ffdd0bea31ba7080e2cd4917358c031644
Shuang0420 <252618408@qq.com>
2016-09-19 20:47:01 -0400

    [98. Validate Binary Search Tree][Accepted]committed by LeetCode Extension

---
+++ 98-Validate-Binary-Search-Tree/solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        return self.helper(root, float('-inf'), float('inf'))
+        
+    def helper(self, root, min, max):
+        if not root:
+            return True
+        if root.val >= max or root.val <= min:
+            return False
+        return self.helper(root.left, min, root.val) and self.helper(root.right, root.val, max)
+        

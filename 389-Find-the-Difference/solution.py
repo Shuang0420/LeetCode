778bafaf90c0c47d5e1f22f5d023c06d9b7dc835
Shuang0420 <252618408@qq.com>
2016-09-24 16:08:40 -0400

    [389. Find the Difference][Accepted]committed by LeetCode Extension

---
+++ 389-Find-the-Difference/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        return chr(reduce(lambda x, y: x ^ y, map(ord, s + t)))

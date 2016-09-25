1fd059886a7c3e515d3599863e225e2836b9bcd2
Shuang0420 <252618408@qq.com>
2016-09-25 19:55:50 -0400

    [8. String to Integer (atoi)][Accepted]committed by LeetCode Extension

---
+++ 8-String-to-Integer-(atoi)/solution.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        if not str:
+            return 0
+        sum,sign=0,1
+        i=0
+        maxint=(1<<31)-1
+        str=str.strip()
+        if str[0]=='+':
+            i=1
+        if str[0]=='-':
+            sign=-1
+            i=1
+        
+        num=0
+        for s in range(i,len(str)):
+            if str[s] < '0' or str[s] > '9':
+                break
+            num=num*10+int(str[s])
+            if num>maxint:
+                break
+            
+        num*=sign
+        if num>maxint:
+            return maxint
+        if num<-1*maxint:
+            return -1*maxint-1
+        return num
+            
+        

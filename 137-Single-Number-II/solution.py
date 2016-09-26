46abe8295bfe615d25f3ba4d60bc42f4e20fe6f3
Shuang0420 <252618408@qq.com>
2016-09-25 21:24:49 -0400

    [137. Single Number II][Accepted]committed by LeetCode Extension

---
+++ 137-Single-Number-II/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return nums
+        nums=sorted(nums)
+        for i in range(1,len(nums),3):
+            if nums[i]!=nums[i-1] or nums[i]!=nums[i+1]:
+                return reduce(lambda x,y:x^y,nums[i-1:i+2])
+        return nums[-1]

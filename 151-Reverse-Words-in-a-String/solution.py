3978b2fe20889e11aa885ddcc5b7d1a95bfe461d
Shuang0420 <252618408@qq.com>
2016-09-27 21:42:12 -0400

    [151. Reverse Words in a String][Accepted]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.py
+++ 151-Reverse-Words-in-a-String/solution.py
@@ -4,8 +4,4 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        if not s:
-            return s
-        s=s.strip()
-        words=re.split('\s+',s)
-        return ' '.join(words[::-1])
+        return ' '.join(s.strip().split()[::-1])

63b3ddb90cefaea35e374f96065be234c33b90d7
Shuang0420 <252618408@qq.com>
2016-10-07 23:33:43 -0400

    [170. Two Sum III - Data structure design][Accepted]committed by LeetCode Extension

--- 170-Two-Sum-III---Data-structure-design/solution.py
+++ 170-Two-Sum-III---Data-structure-design/solution.py
@@ -1,3 +1,17 @@
+'''
+About hashmap:
+- Do not use dict.keys! 
+In Python 2 dict.keys() creates the whole list of keys first that's why it is an O(N) operation, while key in dict is an O(1) operation.
+>>> dic = dict.fromkeys(range(10**5))
+>>> %timeit 10000 in dic
+1000000 loops, best of 3: 170 ns per loop
+>>> %timeit 10000 in dic.keys()
+100 loops, best of 3: 4.98 ms per loop
+>>> %timeit 10000 in dic.iterkeys()
+1000 loops, best of 3: 402 us per loop
+>>> %timeit 10000 in dic.viewkeys()
+1000000 loops, best of 3: 457 ns per loop
+'''
 class TwoSum(object):
 
     def __init__(self):
@@ -11,7 +25,11 @@ class TwoSum(object):
         Add the number to an internal data structure.
         :rtype: nothing
         """
-        self.hashmap[number]=self.hashmap.get(number,0)+1
+        if self.hashmap.has_key(number):
+            self.hashmap[number]+=1
+        else:
+            self.hashmap[number]=1
+        # self.hashmap[number]=self.hashmap.get(number,0)+1
 
     def find(self, value):
         """

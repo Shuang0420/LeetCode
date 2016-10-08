63bbacfbf4b3cdccf52bf0e6068061f937db0554
Shuang0420 <252618408@qq.com>
2016-10-07 20:58:44 -0400

    [1. Two Sum][Accepted]committed by LeetCode Extension

--- 1-Two-Sum/solution.py
+++ 1-Two-Sum/solution.py
@@ -4,35 +4,54 @@ Check assumption:
 - each input would have exactly one solution
 - duplicates in array
 
-Solution:
-1. Sort array, use start,end pointers to search target 9. Time complexity: O(nlogn)
-2. Loop array, find if the remain of current number exists in the array(Don't loop current number). Time complexity: O(n^2)
+Corner case:
+- len(nums)<2 or nums==None
 
-Follow-up:
-- Minimize time complexity: Use hashmap to store array index and value, so that the second loop for solution2 will have O(1) time complexity, and the total complexity would be O(n). The cost is space complexity.
+Solution:
+- Loop array, search target-nums[i] for each nums[j] on the right. Time complexity: O(n^2)
+
+Attention:
+- we cannot sort array, compare target and sum and move pointers to get the answer as it would disrupt the order
+- array.index(value) returns first match, but this may not be what you expect
+
+Optimization:
+- While loop, use hashmap<target-nums[i],i> to store remaining index and value, so that the second loop will have O(1) time complexity, and the total complexity would be O(n). The cost is space complexity. This is two-pass solution.
+- Two-pass --> One pass. While loop, for each i, check if it is in hashmap, if not, add it, if exists, return index.
 '''
 
 class Solution(object):
+    
+    # with hashmap
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
+        if not nums or len(nums)<2:
+            return None
+        hashmap=dict()
+        for index,value in enumerate(nums):
+            if hashmap.has_key(target-value):
+                return[index,hashmap[target-value]]
+            else:
+                hashmap[value]=index
+        return None
+    
+    
+    '''
+    # two loops
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums or len(nums)<2:
+            return None
         for index1,value in enumerate(nums):
             for index2 in range(index1+1,len(nums)):
                 if nums[index2]==target-value:
                     return [index1,index2]
-        return None
-        
-    '''
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
-
         return None
     '''

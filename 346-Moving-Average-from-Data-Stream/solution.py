c1de73bbb2803c31abfde09f48c6b232a64f583b
Shuang0420 <252618408@qq.com>
2016-10-08 09:13:32 -0400

    [346. Moving Average from Data Stream][Accepted]committed by LeetCode Extension

---
+++ 346-Moving-Average-from-Data-Stream/solution.py
@@ -0,0 +1,27 @@
+from collections import deque
+class MovingAverage(object):
+
+    def __init__(self, size):
+        """
+        Initialize your data structure here.
+        :type size: int
+        """
+        self.q=deque()
+        self.size=size
+
+    def next(self, val):
+        """
+        :type val: int
+        :rtype: float
+        """
+        self.q.append(val)
+        avg=sum(self.q)/float(len(self.q))
+        if len(self.q)==self.size:
+            self.q.popleft()
+        return avg
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)

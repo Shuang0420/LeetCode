634f41d29dc7842a397da52ea265ed0fbad0fa8d
Shuang0420 <252618408@qq.com>
2016-10-08 11:22:51 -0400

    [75. Sort Colors][Accepted]committed by LeetCode Extension

---
+++ 75-Sort-Colors/solution.py
@@ -0,0 +1,34 @@
+'''
+Solution:
+- quicksort?  Time complexity: O(nlogn)
+- 3 numbers, so count and reset, Time complexity: O(n), Space complexity: O(3)->O(1), two-pass
+- subarray with different states, Time complexity: O(n)
+
+Attention:
+- for solution 3, boundary(pointer is not included)
+'''
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if len(nums)<1:
+            return
+        a0,a1,a2=0,0,0
+        for n in nums:
+            if n==0: a0+=1
+            elif n==1: a1+=1
+            elif n==2: a2+=1
+        for i in range(len(nums)):
+            if a0>0: 
+                nums[i]=0
+                a0-=1
+            elif a1>0:
+                nums[i]=1
+                a1-=1
+            elif a2>0:
+                nums[i]=2
+                a2-=1
+            
+                
